1e4d091942a8f87970f8df82f68905594bd06c8d
Michael Fegreus <michael.fegreus@gmail.com>
2017-03-09 15:15:10 -0500

    Imported test sprites and rigs for 2D and 3D.

---
+++ Assets/Scripts/Player Scripts/scr_anim3d_test.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_anim3d_test : MonoBehaviour {
+
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		anim.SetFloat("Speed", Input.GetAxis ("Horizontal"));
+		anim.SetFloat("Speed", Input.GetAxis ("Vertical"));
+	}
+}
---
+++ Assets/Scripts/Player Scripts/scr_anim_test.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_anim_test : MonoBehaviour {
+
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		anim.SetFloat("inputX", Input.GetAxis ("Horizontal"));
+		anim.SetFloat("inputY", Input.GetAxis ("Vertical"));
+	}
+}
