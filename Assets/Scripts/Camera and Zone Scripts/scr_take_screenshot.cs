135efcea25aabede262dfe023e643ccc2d917089
Michael Fegreus <michael.fegreus@gmail.com>
2017-05-16 18:50:21 -0400

    Student show part 1

---
+++ Assets/Scripts/Camera and Zone Scripts/scr_take_screenshot.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_take_screenshot : MonoBehaviour {
+
+	public string screenshotName = "screenshot";
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.P)) {
+			Application.CaptureScreenshot (screenshotName + ".png", 2);
+		}
+	}
+}
---
+++ Assets/Scripts/Player Scripts/scr_3d_anim_test.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_3d_anim_test : MonoBehaviour {
+
+	Animator anim;
+
+	float inputX;
+	float inputY;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		inputX = Input.GetAxis("Horizontal");
+		inputY = Input.GetAxis("Vertical");
+
+		if (inputX < -.001f || inputX > .001f || inputY > .001f || inputY < -.001f) {
+			anim.SetBool ("walk", true);
+		} else {
+			anim.SetBool ("walk", false);
+		}
+
+		//anim.SetFloat("inputX", Input.GetAxis ("Horizontal"));
+		//anim.SetFloat("inputY", Input.GetAxis ("Vertical"));
+	}
+}
