1c14b90f5399af6113af7ac3745982e48db68a39
Michael Fegreus <michael.fegreus@gmail.com>
2017-08-21 23:07:07 -0400

    Wrote video capture script (modified screencap script) for social media. Did a test of an isometric area using a painting from the internet. Didn't look that great. Did a nice looking texture job on the interior lanterns, with specular lighting, bump mapping, and transparency mapping - first good job at a prop texture! Lastly, I'm getting close to 300 followers on Tumblr, which means I'm that much closer to finding an audience for the game!

---
+++ Assets/Scripts/Camera and Zone Scripts/scr_take_video.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_take_video : MonoBehaviour {
+
+	public string screenshotName = "screenshot";
+	int frameNum = 1;
+
+	// Update is called once per frame
+	void Update () {
+		ScreenCapture.CaptureScreenshot (screenshotName + frameNum + ".png", 2);
+		frameNum++;
+	}
+}
