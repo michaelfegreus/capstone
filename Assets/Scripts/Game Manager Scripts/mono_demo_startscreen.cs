619c641deec61686792cd2437f3368c3e349bfa0
Michael Fegreus <michael.fegreus@gmail.com>
2017-11-22 19:13:50 -0800

    Syncing demo from Megabooth submission.

---
+++ Assets/Scripts/Game Manager Scripts/mono_demo_startscreen.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mono_demo_startscreen : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Return) || Input.GetKeyDown (KeyCode.KeypadEnter)) {
+			Destroy (gameObject);
+		}
+	}
+}
